a1fc2df6eba65716abb4c15b7182ff1d92a99a3a
MarcoA-Dev10 <marcoaurelioprg1@gmail.com>
2025-01-14 19:10:34 -0300

    alteração nos input´s

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const buttonClick = document.querySelector(".button-click")
 
+
+
 function generateNumber(){
     const inputMin = Math.ceil(document.getElementById("input-min").value)
     const inputMax = Math.floor(document.getElementById("input-max").value)
 
-    const result = Math.floor(Math.random() * (inputMax - inputMin) + inputMin);
+    if(inputMin >= inputMax){
+        alert("O valor minimo tem que ser MENOR que o valor Maximo!")
+    }else{
+        const result = Math.floor(Math.random() * (inputMax - inputMin) + inputMin);
 
-    alert(result)
+        alert(result)
+    }
 }
+
 
 
 buttonClick.addEventListener("click", generateNumber)
